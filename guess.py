767f1827f5f18857bd7b025d6f92fb8b036530b2
Mihir Patel <patelm48@student.wpunj.edu>
2017-04-26 18:19:55 -0700

    add more structure to the guess game.

--- guess.py
+++ guess.py
@@ -2,22 +2,39 @@
 
 import random
 
-# implement a guess a number game within a range
+###################################################################
+# Author : Mihir Patel
+#   Date : April 26, 2017
+#   Desc : implement an interactive  "Guess a number" game.
+###################################################################
 
 print ("+++++++++++++++++++++++++++++++++++")
 print ("Welcome to the guess a number game!")
 print ("+++++++++++++++++++++++++++++++++++")
 print ("")
 print ("")
-random_num = random.choice(range(1,11))
+
+LOW=1
+HI=11
+random_num = random.choice(range(LOW,HI))
+
+# keep asking the user until the right guess is provided
 while (True):
     guess = str(input("Guess a number between 1 and 10  >"))
-    if not guess.isdigit():
-        continue
-    else:
-        guess = int(guess)
 
-    if guess == random_num:
+    # handle invalid input
+    if not guess.isdigit():
+        print ("Invalid input...You Lose!!!")
+        break
+
+    guess = int(guess)
+    if guess < random_num:
+        print ("Too Low...")
+        print ("Try again!")
+    elif guess > random_num:
+        print ("Too high...")
+        print ("Try again!")
+    else:
         print ("You win!!!")
         break
 
