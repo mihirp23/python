8ffc27e38a9fd20b128a377cecbf3bfffa4d9bd9
Mihir <mihirp38661@gmail.com>
2019-02-23 13:35:11 -0500

    Add nba_teams.py

---
+++ nba_teams.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+
+## Mihir Patel
+## 2/21/19
+## File : nba_teams.py
+
+from nba_api.stats.static import teams as Teams
+
+############################################################
+def main():
+############################################################
+
+    teams = Teams.get_teams()
+
+    # go through each team
+    for team in sorted(teams):
+        print "-" * 60
+        for key, value in team.items():
+            # print all key value information
+            # pertaining to teams(except id)
+            if key != 'id':
+                print "%-15s" % (str(key),) + ": " + str(value)
+
+# main()
+
+if __name__ == "__main__":
+    main()
