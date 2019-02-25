f76a0fbb1175bffac44879efbb296ab33caf4678
mihirp23 <mihirp38661@gmail.com>
2019-02-25 14:29:21 -0500

    Add does_dir_exist.py

---
+++ does_dir_exist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+## Mihir Patel
+## 2/25/19
+## A program that determines if a directory 
+## exists.
+## File : does_dir_exist.py
+## Version : Python2
+
+import os
+
+#################################################################
+def main():
+#################################################################
+    if os.path.isdir('/tmp'):
+        print "Directory exists."
+    else:
+        print "Directory does not exists."
+# main()
+
+if __name__ == "__main__":
+    main()
+[misdev:bnha
