e3a8d2306f4d2ffb6e0899b9b8a23787e4d904ef
mihirp23 <mihirp38661@gmail.com>
2019-03-13 08:17:47 -0400

    Add stat_funcs.py

---
+++ stat_funcs.py
@@ -0,0 +1,13 @@
+## Mihir Patel
+## 3/13/19
+## stat_funcs.py
+
+
+# use functions from statistics module
+
+from statistics import mean
+from statistics import median
+
+lst = range(0, 101)
+print("  Mean: " + str(mean(lst)))
+print("Median: " + str(median(lst)))
